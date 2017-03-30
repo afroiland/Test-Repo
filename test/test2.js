4bb00ddebb2f894c186126751eac0973655589cd
Andrew Froiland <andrew.froiland@gmail.com>
2017-03-30 16:23:19 -0500

    Complete increment function

--- test/test2.js
+++ test/test2.js
@@ -8,3 +8,11 @@ console.log('newAdd(3, 4): ', newAdd(3, 4));
 
 let fff = () => console.log('fff got called');
 fff();
+
+setTimeout( () => console.log('2 second timeout'), 2000);
+
+function increment() {
+  this.number = 0;
+  setInterval( () => {this.number++; console.log(this.number);}, 1000);
+}
+// let incr = new increment();
